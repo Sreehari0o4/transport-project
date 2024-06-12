372ee5e75010ba3895e82f7d6b5334008f638d02
snehasivak <118305223+snehasivak@users.noreply.github.com>
2024-06-13 01:47:38 +0530

    Add files via upload
    manufacturer interface

---
+++ interface.js
@@ -0,0 +1,9 @@
+function goToLoginPage() {
+    window.location.href = 'login.html'; // Replace 'home.html' with the actual URL of your home page
+  }
+  
+  function redirectToOrders() {
+    window.location.href = "orders.html"; // Replace with the URL of your orders management page
+}
+
+
